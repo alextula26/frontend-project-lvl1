51d05f906c352965a3602595a96ad633ca43f107
alextula26 <alex_marc86@mail.ru>
2020-03-04 23:01:57 +0300

    rename welcome on greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import welcome from '../src/index.js';
+import greeting from '../src/index.js';
 console.log('Welcome to the Brain Games!');
-console.log('Hello, ' + welcome() + '!');
+console.log('Hello, ' + greeting() + '!');
 
