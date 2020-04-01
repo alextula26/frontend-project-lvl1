b8c6f7f41dd65a5323fe972b08ce445d8f627782
alextula26 <alex_marc86@mail.ru>
2020-04-01 11:01:14 +0300

    change settings in eslintrc.yml and update createProgression function

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,12 +1,12 @@
 import gameEngine from '../index.js';
 import getRandomNumber from '../random.js';
 
-const createProgression = (start, step, index, length, char) => {
+const createProgression = (start, step, index, length) => {
   const iter = (value, count, acc) => {
     if (count === length) {
       return acc;
     }
-    return iter(value + step, count + 1, [...acc, count !== index ? value : char]);
+    return iter(value + step, count + 1, [...acc, count !== index ? value : '..']);
   };
 
   return iter(start, 0, []);
@@ -15,14 +15,12 @@ const createProgression = (start, step, index, length, char) => {
 const gameDescription = 'What number is missing in the progression?';
 
 const generateGame = () => {
-  const symbolReplace = '..';
   const length = 10;
   const startValue = getRandomNumber(1, length);
   const step = getRandomNumber(1, length);
   const hiddenElementIndex = getRandomNumber(0, length - 1);
   const hiddenElementValue = startValue + (step * hiddenElementIndex);
-
-  const progression = createProgression(startValue, step, hiddenElementIndex, length, symbolReplace);
+  const progression = createProgression(startValue, step, hiddenElementIndex, length);
   const gameResult = String(hiddenElementValue);
   const gameQuestion = progression.join(' ');
 
