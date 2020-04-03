fe718c0d8cd0f109d74da0e1fe4f35ea3bb7dfc9
alextula26 <alex_marc86@mail.ru>
2020-04-03 13:01:21 +0300

    refactoring progression function

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,12 +1,12 @@
 import gameEngine from '../index.js';
 import getRandomNumber from '../random.js';
 
-const createProgression = (start, step, index, length) => {
+const progression = (start, step, index, length, char) => {
   const iter = (value, count, acc) => {
     if (count === length) {
       return acc;
     }
-    return iter(value + step, count + 1, [...acc, count !== index ? value : '..']);
+    return iter(value + step, count + 1, [...acc, count !== index ? value : char]);
   };
 
   return iter(start, 0, []);
@@ -15,14 +15,13 @@ const createProgression = (start, step, index, length) => {
 const gameDescription = 'What number is missing in the progression?';
 
 const generateGame = () => {
+  const symbolReplace = '..';
   const length = 10;
   const startValue = getRandomNumber(1, length);
   const step = getRandomNumber(1, length);
   const hiddenElementIndex = getRandomNumber(0, length - 1);
-  const hiddenElementValue = startValue + (step * hiddenElementIndex);
-  const progression = createProgression(startValue, step, hiddenElementIndex, length);
-  const gameResult = String(hiddenElementValue);
-  const gameQuestion = progression.join(' ');
+  const gameResult = String(startValue + (step * hiddenElementIndex));
+  const gameQuestion = progression(startValue, step, hiddenElementIndex, length, symbolReplace).join(' ');
 
   return [gameResult, gameQuestion];
 };
